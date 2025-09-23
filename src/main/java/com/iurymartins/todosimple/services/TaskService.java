f2d39030e9f603bf1fcf10b5164b90adc170374b
IuryRayam <iury2155@gmail.com>
2025-09-23 18:52:36 -0300

    Create TaskService

---
+++ src/main/java/com/iurymartins/todosimple/services/TaskService.java
@@ -0,0 +1,56 @@
+package com.iurymartins.todosimple.services;
+
+import java.util.Optional;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.iurymartins.todosimple.models.Task;
+import com.iurymartins.todosimple.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id) {
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException(
+            "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Task create(Task obj) {
+        User user = (User) this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser((com.iurymartins.todosimple.models.User) user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj) {
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                "Não é possível excluir a tarefa. Pois há tarefas associadas a ela."
+            );
+        }
+    }
+}
