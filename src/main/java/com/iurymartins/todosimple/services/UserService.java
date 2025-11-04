855bd95caf66cfb13bfe204bd6ad427ea1276485
IuryRayam <iury2155@gmail.com>
2025-11-04 17:43:16 -0300

    Set UserService authorization

--- src/main/java/com/iurymartins/todosimple/services/UserService.java
+++ src/main/java/com/iurymartins/todosimple/services/UserService.java
@@ -1,10 +1,12 @@
 package com.iurymartins.todosimple.services;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.iurymartins.todosimple.models.User;
 import com.iurymartins.todosimple.models.enums.ProfileEnum;
 import com.iurymartins.todosimple.repositories.UserRepository;
+import com.iurymartins.todosimple.security.UserSpringSecurity;
+import com.iurymartins.todosimple.services.exceptions.AuthorizationException;
 import com.iurymartins.todosimple.services.exceptions.DataBindingViolationException;
 import com.iurymartins.todosimple.services.exceptions.ObjectNotFoundException;
 
@@ -25,6 +29,11 @@ public class UserService {
     private UserRepository userRepository;
 
     public User findById(Long id) {
+        UserSpringSecurity userSpringSecurity = authenticated();
+        if (!Objects.nonNull(userSpringSecurity) 
+                || !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !id.equals(userSpringSecurity.getId()))
+            throw new AuthorizationException("Acesso negado!");
+
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException(
             "usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
@@ -56,6 +65,14 @@ public class UserService {
             throw new DataBindingViolationException(
                 "Não é possível excluir pois há entidades relacionadas!"
             );
+        }
+    }
+
+    public static UserSpringSecurity authenticated() {
+        try {
+            return (UserSpringSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            return null;
         }
     }
 
